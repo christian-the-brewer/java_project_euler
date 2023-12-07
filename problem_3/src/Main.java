7bdc896460b59ad933b06f21b36289b7e1c9fe0e
christian-the-brewer <brewer.christiang@gmail.com>
2023-12-06 22:04:41 -0500

    stopping for the night

--- problem_3/src/Main.java
+++ problem_3/src/Main.java
@@ -1,5 +1,20 @@
+//Largest Prime Factor Problem 3
+//Author: Christian Brewer
+//Date 23-12-6
+//What is the largest prime factor of 600851475143
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        long target = 600_851_475_143L;
+
+        for (long i = target / 2; i > 0; i--) {
+
+        }
+
     }
 }
+
+//To find the largest, factors, we could divide the target in half, then begin checking for factors of every add number
+//since 2 is the only prime in this set.
+
+//OR start by finding primes up to target and then just check in reverse order for the first factor. I like this one better.
